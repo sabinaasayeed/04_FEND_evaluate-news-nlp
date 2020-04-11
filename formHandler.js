2ad55e7ca3c6df6ba51dcb764a26739229ba6224
sabinaasayeed <59181067+sabinaasayeed@users.noreply.github.com>
2020-04-11 16:01:31 -0400

    Updated the project to use URL for API call

---
+++ formHandler.js
@@ -0,0 +1,36 @@
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let formUrl = document.getElementById('name').value
+    if (Client.checkForName(formUrl)) {
+        console.log("::: Form Submitted :::")
+        let data = { url: formUrl };
+        fetch("/test", {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(function(res) {
+                console.log("::: response :::", res)
+                document.getElementById('results').innerHTML =
+                    `<h2>Results for : ${formUrl}</h2>
+                    <ul style="list-style-type:square;">
+                    <li>Polarity:    <var>${res.polarity}</var> </li>
+                    <li>Subjectivity:    <var>${res.subjectivity}</var> </li>
+                    <li>Polarity confidence:    <var>${res.polarity_confidence}</var> </lir>
+                    <li>Subjectivity confidence:    <var>${res.subjectivity_confidence}</var></li>
+                    </ul>`;
+            })
+    }
+
+
+}
+
+export { handleSubmit }
